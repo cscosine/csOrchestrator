4a6ab2a37dcf4ed95a521625694bc82e55474b00
cscosine-dev <cscosine@gmail.com>
2026-04-23 19:17:38 +0200

    add step for repository with possible extras

--- src/csorchestrator/step/step_add_repository.py
+++ src/csorchestrator/step/step_add_repository.py
@@ -1,8 +1,37 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Dict, Optional, Type, TypeVar
 
 from csorchestrator.orchestrator.step_base import StepBase
 
 
+# base class for extra information that can be provided
+class StepAddRepositoryExtra:
+    pass
+
+
+@dataclass
+class StepAddRepositoryExtraAccessToken(StepAddRepositoryExtra):
+    token_name: str
+
+
+T = TypeVar("T", bound="StepAddRepositoryExtra")
+
+
 @dataclass
 class StepAddRepository(StepBase):
-    pass
+    target_directory: str
+    repo_url: str
+    ref: str
+    _extras: Dict[type, StepAddRepositoryExtra] = field(default_factory=dict)
+
+    def add_extra(
+        self,
+        extra: StepAddRepositoryExtra,
+    ) -> "StepAddRepository":
+        key = type(extra)
+        self._extras[key] = extra
+        return self
+
+    def get_extra(self, t: Type[T]) -> Optional[T]:
+        extra = self._extras.get(t)
+        return extra if isinstance(extra, t) else None
