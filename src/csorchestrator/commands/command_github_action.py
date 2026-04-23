ee0d2e902e1e94a901a34251c9afcd67d7727263
cscosine-dev <cscosine@gmail.com>
2026-04-23 18:46:21 +0200

    remove unused commands

--- src/csorchestrator/commands/command_github_action.py
+++
@@ -1,2 +0,0 @@
-class CommandGithubAction:
-    pass
--- src/csorchestrator/commands/command_text_description.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class CommandTextDescription:
-    name: str
-    description: str
-    text: str
