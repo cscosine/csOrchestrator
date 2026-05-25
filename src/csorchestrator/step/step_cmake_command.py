0956eaac262d1fabbc16ffbef34caf7cfcc6ac1d
cscosine-dev <cscosine@gmail.com>
2026-05-26 00:05:40 +0200

    prepare cmake step to exec on matrix

--- src/csorchestrator/step/step_cmake_command.py
+++ src/csorchestrator/step/step_cmake_command.py
@@ -4,11 +4,14 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable, TextIO
 
+from csorchestrator.context.context_compiler_generator import ContextCompilerGenerator
 from csorchestrator.context.context_local_execution import ContextLocalExecution
+from csorchestrator.context.context_os_architecture import ContextOsArchitecture
 from csorchestrator.core.report import Report
 from csorchestrator.orchestrator.reporter_sink_base import ReporterSinkBase
 from csorchestrator.orchestrator.step_base import StepBase, StepExecuteOnMatchingContext
 from csorchestrator.utils.presets.supported_variants import (
+    BuildConfig,
     ContextOsArchitectureCompilerGeneratorConfig,
     workflow_name_from_description,
 )
@@ -17,7 +20,28 @@ from csorchestrator.utils.presets.supported_variants import (
 @dataclass
 class StepCMakeWorkflow(StepBase):
     source_dir: str
-    workflow_description: ContextOsArchitectureCompilerGeneratorConfig
+
+    config: BuildConfig
+
+    context_os_architecture: ContextOsArchitecture | None = None
+    context_compiler_generator: ContextCompilerGenerator | None = None
+
+    @classmethod
+    def create_from_workflow_description(
+        cls,
+        name: str,
+        description: str,
+        source_dir: str,
+        workflow_description: ContextOsArchitectureCompilerGeneratorConfig,
+    ) -> "StepCMakeWorkflow":
+        return cls(
+            name=name,
+            description=description,
+            source_dir=source_dir,
+            context_os_architecture=workflow_description.context_os_architecture,
+            context_compiler_generator=workflow_description.context_compiler_generator,
+            config=workflow_description.config,
+        )
 
 
 def execute_step_cmake_workflow(
@@ -25,11 +49,32 @@ def execute_step_cmake_workflow(
 ) -> Report:
     report = Report()
 
-    workflow_name = workflow_name_from_description(step.workflow_description)
+    context_os_architecture = step.context_os_architecture
+    if context_os_architecture is None:
+        context_os_architecture = context.os_architecture
+    if context_os_architecture is None:
+        report.append_error(
+            f"context_os_architecture is None for {step.name}, please provide it in the execution matrix or in the step"
+        )
+        return report
+
+    context_compiler_generator = step.context_compiler_generator
+    if context_compiler_generator is None:
+        context_compiler_generator = None  # TODO how to retrieve from context?!?
+    if context_compiler_generator is None:
+        report.append_error(
+            f"context_compiler_generator is None for {step.name},"
+            "please provide it in the execution matrix or in the step"
+        )
+        return report
+
+    workflow_name = workflow_name_from_description(
+        ContextOsArchitectureCompilerGeneratorConfig(context_os_architecture, context_compiler_generator, step.config)
+    )
 
     excute_on_matching_context = step.get_extra(StepExecuteOnMatchingContext)
     if excute_on_matching_context is not None:
-        match = step.workflow_description.context_os_architecture.is_equal_to(context.os_architecture)
+        match = context_os_architecture.is_equal_to(context.os_architecture)
         if not match:
             report.append_info(f"Skip '{workflow_name}', not compatible with the current context")
             return report
